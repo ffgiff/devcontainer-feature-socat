47ea4da12eb5bedf0d6661cf8cdc744ffcbe2f8a
fgiff <ffgiff@gmail.com>
2026-06-18 14:59:44 +0100

    Refactor to use an entrypoint script

--- src/install.sh
+++ src/install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+mkdir -p /opt/devcontainer-feature-socat
+cat > /opt/devcontainer-feature-socat/entrypoint.sh <<END
 for PORT in SOCAT_PORTS
 do
     socat TCP-LISTEN:$PORT,fork,reuseaddr TCP:host.containers.internal:$PORT &
 done
+END
+
+# Create the entrypoint script
+chmod +x /opt/devcontainer-feature-socat/entrypoint.sh \
+    && chown ${USERNAME}:root /opt/devcontainer-feature-socat/entrypoint.sh
